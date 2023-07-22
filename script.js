c00814ecd36027755c0c095ab1138b7ce1bd58a3
black-RAM <pplanes311@gmail.com>
2023-07-22 18:43:11 +0300

    renamed redo.js script.js, deleted old file

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-// Game board factory IIFE, a singleton object
 const Game = (() => {
-  // board stored as a 2D array
   const board = [
     [],
     [],
     []
   ];
 
-  function initializeBoard(){
-    // populate grid with null
-    for (let row of board) {
-      for(let i = 0; i < 3; i++){
+  function initialiseBoard() {
+    for (const row of board) {
+      for (let i = 0; i < 3; i++) {
         row.push(null);
       }
     }
-    emptyCells = getEmptyCells(board);
     notify();
   }
 
-  function getEmptyCells(grid) {
+  function getEmptyCells() {
     let coordinates = [];
-    for (let row = 0; row < grid.length; row++) {
-      for (let col = 0; col < grid[row].length; col++) {
-        if (grid[row][col] === null) {
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col < board[row].length; col++) {
+        if (board[row][col] === null) {
           coordinates.push([row, col]);
         }
       }
@@ -30,119 +26,94 @@ const Game = (() => {
     return coordinates;
   }
 
-  let emptyCells = getEmptyCells(board);
-
   function makeMove(row, col, symbol) {
-    let cell = board[row][col];
-    let valid = cell === null;
-
+    let valid = board[row][col] === null;
     if (valid) {
       board[row][col] = symbol;
-      emptyCells = getEmptyCells(board);
-      notify();
+      notify(); 
     }
-
     return valid;
   }
 
-  // observer pattern
-  let observers = [];
+  const observers = [];
   function subscribe(observer) {
     observers.push(observer);
   }
-  function notify(){
+
+  function notify() {
     for (const observer of observers) {
-      observer.update(board, emptyCells);
+      observer.update(board);
     }
   }
-  
-  return {board, emptyCells, getEmptyCells, initializeBoard, makeMove, subscribe};
+
+  return { board, getEmptyCells, initialiseBoard, makeMove, subscribe };
 })();
 
-// Player factory
-const Player = (name, symbol, isHuman) => {
-  return {name, symbol, isHuman};
-};
+// player factory
+const Player = ((name, symbol, isHuman) => {
+  return {name, symbol, isHuman}
+});
 const user = Player("Ariel", "X", true);
 const computer = Player("Computer", "O", false);
 
-// controller logic
+// Game Controller
 const GameController = (() => {
   let currentPlayer = null;
   let gameEnded = false;
 
-  function startGame() {
-    Game.initializeBoard();
+  function startGame () {
+    Game.initialiseBoard();
+    currentPlayer = user;
+  }
+
+  function userMove(row, col) {
+    if(gameEnded) return;
+
+    const validMove = Game.makeMove(row, col, currentPlayer.symbol);
+
+    if(validMove) {
+      checkEndGame();
+      currentPlayer = computer;
+      if (!gameEnded) {
+        setTimeout(computerMove, 500); // Delay computer move for 500 milliseconds
+      }
+    }
+  }
+
+  function computerMove() {
+    if(gameEnded || currentPlayer !== computer) return;
+
+    // play random coordinates from empty coordinates
+    const emptyCells = Game.getEmptyCells();
+    let randomPick = Math.floor(Math.random() * emptyCells.length);
+    let [row, col] = emptyCells[randomPick];
+    Game.makeMove(row, col, currentPlayer.symbol);
+
+    checkEndGame()
+
     currentPlayer = user;
   }
 
   function checkEndGame() {
-    // logic for terminating conditions
-  }
-
-  function userMove (row, col) {
-    if(gameEnded) return;
-    Game.makeMove(row, col, user.symbol);
-
-    // check for a win or draw
-    checkEndGame();
-
-    // Switch to computer's turn
-    currentPlayer = computer;
-  }
-
-  function computerMove() {
-    // Get the latest empty cells array and make the computer move
-    const emptyCells = [Game.emptyCells];
+    // Draw condition
+    const emptyCells = Game.getEmptyCells();
     if (emptyCells.length === 0) {
-      // No empty cells left, end the game
       gameEnded = true;
-      return;
-    }
-
-    const randomPick = Math.floor(Math.random() * emptyCells.length);
-    const [row, col] = emptyCells[randomPick];
-    Game.makeMove(row, col, computer.symbol);
-
-    // Check for a winner or a draw after the computer's move
-    checkEndGame();
-
-    // Switch to the next player (user)
-    currentPlayer = user;
-  }
-
-  function computerTurn() {
-    if (!gameEnded && currentPlayer === computer) {
-      setTimeout(() => {
-        computerMove();
-      }, 500); // Delay computer move for better user experience
+      console.log("It's a draw!");
     }
   }
 
-  // Observer pattern - store latest state
-  function update(board) {
-    Game.board = board;
-
-    // Update the emptyCells array with the latest coordinates
-    Game.emptyCells = Game.getEmptyCells(board);
-
-    // trigger computer's turn
-    computerMove();
-  }
-  return {update, startGame, userMove}
+  return {startGame, userMove};
 })();
-Game.subscribe(GameController);
 
-// view
+// Game View
 const GameView = (() => {
   function update(board) {
-    // update DOM
-    console.log(board);
+    console.log(board)
   }
   return {update};
-})();
-Game.subscribe(GameView);
+})()
+Game.subscribe(GameView)
 
-// test the start game method
 GameController.startGame();
 GameController.userMove(1, 1);
